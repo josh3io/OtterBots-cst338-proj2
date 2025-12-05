75f9ba5aa222f49edb83db55206afb448a6a50c8
Chris <140977972+Cbuen@users.noreply.github.com>
2025-12-05 00:27:46 -0800

    Created npcPlay() logic for computer to generate a roll of Rock, Paper, or Scissors. This guess will be used to display on UI and determine user vs npc win or lose outcome

--- app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/GamePlayActivity.java
+++ app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/GamePlayActivity.java
@@ -1,6 +1,7 @@
 package edu.csumb.cst338.otterbots.rockpaperscissors;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -9,23 +10,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 import edu.csumb.cst338.otterbots.rockpaperscissors.databinding.ActivityGamePlayBinding;
 
 public class GamePlayActivity extends AppCompatActivity {
 
+    Random random = new Random();
+    private HashMap<Integer, String> GAME_CHOICES = new HashMap<>();
+    private String npcCurrentGuess = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        GAME_CHOICES.put(0, "ROCK");
+        GAME_CHOICES.put(1, "PAPER");
+        GAME_CHOICES.put(2, "SCISSORS");
 
        ActivityGamePlayBinding binding = ActivityGamePlayBinding.inflate(getLayoutInflater());
        setContentView(binding.getRoot());
        setupUserUI(binding);
     }
 
-    public void setupUserUI(ActivityGamePlayBinding binding) {
+    private void setupUserUI(ActivityGamePlayBinding binding) {
         binding.rockPlayButton.setOnClickListener((v) -> {
             // TODO: Implement rock play logic
-           toastMaker("Player selected rock");
+            // TODO: REMOVE THIS DEBUG TOAST
+
+            // TODO: Implement npc play, updateNpcGuess, in each onclicklistner
+            npcPlay();
+           toastMaker("NPC GUESS: " + npcCurrentGuess + " Player guess: Rock");
+
         });
 
         binding.paperPlayButton.setOnClickListener((v) -> {
@@ -46,7 +63,19 @@ public class GamePlayActivity extends AppCompatActivity {
 
     }
 
-    public void toastMaker(String message) {
+    // Function generates and sets an npcPlay to be set
+     private void npcPlay() {
+        int npc_guess = random.nextInt(GAME_CHOICES.size());
+        npcCurrentGuess = GAME_CHOICES.get(npc_guess);
+    }
+    // TODO: Handle binding logic to update NPC guess text after user sets their play
+        // Helper function to set text Npc Guess
+    private void updateNpcPlay(ActivityGamePlayBinding binding) {
+
+    }
+
+
+     private void toastMaker(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
