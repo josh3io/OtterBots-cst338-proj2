c0bd4fec78677e4710faf7b287ec36791b565b2f
Chris <140977972+Cbuen@users.noreply.github.com>
2025-12-04 21:50:56 -0800

    Created activity_game_play.xml pre GamePlayActivity.java methods

---
+++ app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/GamePlayActivity.java
@@ -0,0 +1,21 @@
+package edu.csumb.cst338.otterbots.rockpaperscissors;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import edu.csumb.cst338.otterbots.rockpaperscissors.databinding.ActivityGamePlayBinding;
+
+public class GamePlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
