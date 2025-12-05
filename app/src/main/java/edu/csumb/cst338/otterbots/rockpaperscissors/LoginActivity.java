b67aaae557f156838755e6830a7415caaa37497e
AJMartinez7 <ajmartinez88@yahoo.com>
2025-12-04 18:43:27 -0800

    feat: Implement LoginActivity and LandingActivity (user/admin) UI (#13)
    * feat: add initial LoginActivity and XML layout with basic validation and navigation setup
    
    * feat: add LoginActivity and LandingActivity UI with admin/user layouts.

---
+++ app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/LoginActivity.java
@@ -0,0 +1,56 @@
+package edu.csumb.cst338.otterbots.rockpaperscissors;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.csumb.cst338.otterbots.rockpaperscissors.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String EXTRA_USERNAME = "userName";
+    public static final String EXTRA_IS_ADMIN = "isAdmin";
+
+    private ActivityLoginBinding binding;
+    //TODO: Add rps repository declaration here, once repo is implemented.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //TODO: Add repository initialization here, once repo is implemented.
+
+        binding.loginButton.setOnClickListener(v -> verifyUser());
+    }
+
+    private void verifyUser() {
+        String userName = binding.userNameLoginEditText.getText().toString().trim();
+        if (userName.isEmpty()) {
+            toastMaker("Username should not be blank.");
+            return;
+        }
+
+        String userPassword = binding.passwordLoginEditText.getText().toString().trim();
+        if (userPassword.isEmpty()) {
+            toastMaker("Password should not be blank.");
+            return;
+        }
+
+        //TODO: Replace following code with real DB-based verification once repo is ready.
+        Intent intent = new Intent(LoginActivity.this, LandingActivity.class);
+        intent.putExtra(EXTRA_USERNAME, userName);
+
+        //TODO: Replace later with real DB isAdmin check.
+        boolean isAdmin = userName.equalsIgnoreCase("admin");
+        intent.putExtra(EXTRA_IS_ADMIN, isAdmin);
+
+        startActivity(intent);
+    }
+
+    private void toastMaker(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
