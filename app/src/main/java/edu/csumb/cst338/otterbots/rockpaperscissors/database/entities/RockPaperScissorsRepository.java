cd7b9a63018740efdb2ec28bfb808984481bb162
Josh Goldberg <josh@3io.com>
2025-12-06 11:18:55 -0800

    add rpsRound access

--- app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/database/entities/RockPaperScissorsRepository.java
+++ app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/database/entities/RockPaperScissorsRepository.java
@@ -3,6 +3,10 @@ package edu.csumb.cst338.otterbots.rockpaperscissors.database.entities;
 import android.app.Application;
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
+
+import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -13,12 +17,15 @@ import edu.csumb.cst338.otterbots.rockpaperscissors.R;
 public class RockPaperScissorsRepository {
     private static RockPaperScissorsRepository repository;
 
+    private RpsRoundDAO rpsRoundDAO;
+
     /**
      * Constructor
      * @param application the android application
      */
     private RockPaperScissorsRepository(Application application) {
         RockPaperScissorsDatabase db = RockPaperScissorsDatabase.getDatabase(application);
+        this.rpsRoundDAO = db.rpsRoundDAO();
     }
 
     /**
@@ -52,4 +59,18 @@ public class RockPaperScissorsRepository {
     }
 
     //TODO: add DAO wrapper methods here; they should all use LiveData<Object> return types
+
+    /**
+     * Get all rps rounds for a userStatsId
+     * @param userStatsId the userStatsId id to use for looking up round records
+     * @return list of records or null ArrayList if none are found
+     */
+    public LiveData<ArrayList<RpsRound>> getAllUserStatsIdRounds(int userStatsId) {
+        return Transformations.map(rpsRoundDAO.getAllRoundsByUserStatsId(userStatsId), list -> {
+            if (list == null) {
+                return new ArrayList<>();
+            }
+            return new ArrayList<>(list);
+        });
+    }
 }
