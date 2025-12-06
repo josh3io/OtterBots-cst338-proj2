3c66a1b9fb75721872ae33dd14b18cd53740844e
Chris <140977972+Cbuen@users.noreply.github.com>
2025-12-05 16:06:12 -0800

    update determineWinner() logic, change each button binding lister to use setter and determineWinner()added fields boolean roundTie, boolean npcWon to handle current rps round

--- app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/GamePlayActivity.java
+++ app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/GamePlayActivity.java
@@ -1,5 +1,6 @@
 package edu.csumb.cst338.otterbots.rockpaperscissors;
 
+import android.net.ipsec.ike.TunnelModeChildSessionParams;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -18,6 +19,11 @@ public class GamePlayActivity extends AppCompatActivity {
     private String userCurrentGuess = "";
     private boolean userWon;
 
+    private boolean npcWon;
+
+    // Check if rps round ends in tie
+    private boolean roundTie;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,30 +38,31 @@ public class GamePlayActivity extends AppCompatActivity {
        setupUserUI(binding);
     }
 
+    // TODO: REMOVE THIS DEBUG TOAST
     private void setupUserUI(ActivityGamePlayBinding binding) {
         binding.rockPlayButton.setOnClickListener((v) -> {
             // TODO: Implement rock play logic
 
             // TODO: Implement npc play, updateNpcGuess, in each onclicklistner
 
-            userCurrentGuess = GAME_CHOICES.get(0);
             setNpcChoice();
-            setUserChoice(userCurrentGuess);
+            setUserChoice("ROCK");
+            determineWinner();
 
             //TODO: Implement result from rps round in UI, updateGameplayUI;
             // another idea is to instead handle UI updates in choice functions?
 
-            // TODO: REMOVE THIS DEBUG TOAST
            toastMaker("NPC GUESS: " + npcCurrentGuess + " Player guess: " + userCurrentGuess);
            toastMaker("USER WON: " + userWon);
 
         });
 
         binding.paperPlayButton.setOnClickListener((v) -> {
-            // TODO: Implement paper play logic
-            userCurrentGuess = GAME_CHOICES.get(1);
             setNpcChoice();
-            setUserChoice(userCurrentGuess);
+            setUserChoice("PAPER");
+            determineWinner();
+
+            //TODO: add updateGameplayUI();
 
             toastMaker("NPC GUESS: " + npcCurrentGuess + " Player guess: " + userCurrentGuess);
             toastMaker("USER WON: " + userWon);
@@ -63,9 +70,9 @@ public class GamePlayActivity extends AppCompatActivity {
 
         binding.scissorsPlayButton.setOnClickListener((v) -> {
             // TODO: Implement scissors play logic
-            userCurrentGuess = GAME_CHOICES.get(1);
             setNpcChoice();
-            setUserChoice(userCurrentGuess);
+            setUserChoice("SCISSORS");
+            determineWinner();
 
             toastMaker("NPC GUESS: " + npcCurrentGuess + " Player guess: " + userCurrentGuess);
             toastMaker("USER WON: " + userWon);
@@ -73,19 +80,12 @@ public class GamePlayActivity extends AppCompatActivity {
 
        binding.returnSelectableTextView.setOnClickListener((v) -> {
           // TODO: Implement return button function
-
-           userCurrentGuess = GAME_CHOICES.get(2);
-           setNpcChoice();
-           setUserChoice(userCurrentGuess);
-
-           toastMaker("NPC GUESS: " + npcCurrentGuess + " Player guess: " + userCurrentGuess);
-           toastMaker("USER WON: " + userWon);
        });
 
 
     }
-    private void setUserChoice(String userCurrentGuess) {
-        userWon = determineWinner(userCurrentGuess, npcCurrentGuess);
+    private void setUserChoice(String userChoice) {
+        userCurrentGuess = userChoice;
     }
 
     // Function generates and sets an npcPlay to be set
@@ -96,26 +96,43 @@ public class GamePlayActivity extends AppCompatActivity {
     
    // TODO: Refractor to update total UI for finsihed round for both user and Npc
     // TODO: Handle binding logic to update NPC guess text after user sets their play
-        // Helper function to set text Npc Guess
-    private void updateNpcPlay(ActivityGamePlayBinding binding) {
 
-    }
     // Helper function to determine of user won or lost (true, false)
-    private boolean determineWinner(String userGuess, String npcGuess) {
-        // determine all winning cases, anything else means user lost
-        if (userGuess.equals(GAME_CHOICES.get(0)) && npcGuess.equals(GAME_CHOICES.get(2))){
-           // user pick rock, npc pick scissor
-            return true;
+    private void determineWinner() {
+        // determine all user winning cases, anything else means user lost
+        if (userCurrentGuess.equals(GAME_CHOICES.get(0)) && npcCurrentGuess.equals(GAME_CHOICES.get(2))){
+           // user pick rock, npc pick scissorA
+            roundTie = false; // redundant but for safety
+            userWon = true;
+            npcWon = false;
+            return;
         }
-        if (userGuess.equals(GAME_CHOICES.get(1)) && npcGuess.equals(GAME_CHOICES.get(0))){
+        if (userCurrentGuess.equals(GAME_CHOICES.get(1)) && npcCurrentGuess.equals(GAME_CHOICES.get(0))){
             // user pick paper, npc pick rock
-            return true;
+            roundTie = false;
+            userWon = true;
+            npcWon = false;
+            return;
         }
-        if (userGuess.equals(GAME_CHOICES.get(2)) && npcGuess.equals(GAME_CHOICES.get(1))){
+        if (userCurrentGuess.equals(GAME_CHOICES.get(2)) && npcCurrentGuess.equals(GAME_CHOICES.get(1))){
             // user pick scissors, npc pick paper
-            return true;
+            roundTie = false;
+            userWon = true;
+            npcWon = false;
+            return;
         }
-        return false;
+        // forgot to include a tie lol
+        if (userCurrentGuess.equals(npcCurrentGuess)) {
+            roundTie = true;
+            userWon = false;
+            npcWon = false;
+            toastMaker("Looks like its a tie");
+            return;
+        }
+        roundTie = false;
+        userWon = false;
+        npcWon = true;
+
     }
 
 
