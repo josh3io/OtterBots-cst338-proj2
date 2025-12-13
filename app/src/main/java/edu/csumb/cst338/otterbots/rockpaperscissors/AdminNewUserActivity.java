b6a0eda6b3823b7948f23388d3875bd6ea98af14
Chris <140977972+Cbuen@users.noreply.github.com>
2025-12-13 01:28:21 -0800

    Created toast maker

--- app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/AdminNewUserActivity.java
+++ app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/AdminNewUserActivity.java
@@ -1,6 +1,7 @@
 package edu.csumb.cst338.otterbots.rockpaperscissors;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import edu.csumb.cst338.otterbots.rockpaperscissors.databinding.ActivityAdminNewUserBinding;
+
 public class AdminNewUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_new_user);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ActivityAdminNewUserBinding binding = ActivityAdminNewUserBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+
+    private void setupUserUI(ActivityAdminNewUserBinding binding) {
+        binding.addUserButton.setOnClickListener(v -> {
+            //TODO: Implement add user functionality
         });
+    }
+
+
+    private void toastMaker(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
