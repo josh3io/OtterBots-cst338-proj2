fe259da1bfb2661d6e6ca74f2920e7e79f95c837
AJMartinez7 <ajmartinez88@yahoo.com>
2025-12-15 15:50:11 -0800

    Refactor admin navigation to use intent factories and rename delete-user activity

--- app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/LandingActivity.java
+++ app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/LandingActivity.java
@@ -121,8 +121,6 @@ public class LandingActivity extends AppCompatActivity {
 
         // Start New Game
         binding.startNewGameButton.setOnClickListener(v -> {
-            // TODO: Replace raw Intent with GamePlayActivity.createIntent(...)
-            //  once that intent factory is implemented in GamePlayActivity.
             Intent intent = GamePlayActivity.gamePlayActivityIntentFactory(this, userId);
             startActivity(intent);
         });
@@ -177,8 +175,7 @@ public class LandingActivity extends AppCompatActivity {
 
         // Delete Users (admin-only feature)
         binding.deleteUserTextView.setOnClickListener(v -> {
-            // TODO: Replace toast with navigation to ADMIN DeleteUserActivity
-            Intent intent = new Intent(getApplicationContext(), adminDeleteuserActivity.class);
+            Intent intent = AdminDeleteUserActivity.createIntent(this);
             startActivity(intent);
 
             toastMaker(getString(R.string.delete_user_admin));
@@ -186,8 +183,7 @@ public class LandingActivity extends AppCompatActivity {
 
         // Add User (admin-only feature)
         binding.addUserTextView.setOnClickListener(v -> {
-            //TODO: Refractor to have intent factory
-            Intent intent = new Intent(this, AdminNewUserActivity.class);
+            Intent intent = AdminNewUserActivity.createIntent(this);
             startActivity(intent);
 
             toastMaker(getString(R.string.toast_add_user_admin));
