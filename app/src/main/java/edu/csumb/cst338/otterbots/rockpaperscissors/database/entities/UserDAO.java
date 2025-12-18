f570794bc0598faabe837160ed889cb7a3bcdaae
Murtaza Badri <mbadri2@student.ohlone.edu>
2025-12-17 22:10:23 -0800

    finished userDAO comments

--- app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/database/entities/UserDAO.java
+++ app/src/main/java/edu/csumb/cst338/otterbots/rockpaperscissors/database/entities/UserDAO.java
@@ -1,4 +1,9 @@
 package edu.csumb.cst338.otterbots.rockpaperscissors.database.entities;
+/**
+ * Description: DAO interface for accessing User data in the Rock-Paper-Scissors application.
+ * Author: Murtaza Badri
+ * Since: 12/07/2025
+ */
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
@@ -6,29 +11,34 @@ import androidx.room.Insert;
 import androidx.room.Query;
 import java.util.List;
 
-
+// Room DAO for User entity
 @Dao
 public interface UserDAO {
-
+    // Insert a new user into the database
   @Insert
   long insert(User user);
 
+  //Retrieve a user by username (observable)
   @Query("SELECT * FROM " + RockPaperScissorsDatabase.USER_TABLE +
       " WHERE username = :username LIMIT 1")
   LiveData<User> getUserByUsername(String username);
 
+  // Retrieve a user for login validation
   @Query("SELECT * FROM " + RockPaperScissorsDatabase.USER_TABLE +
       " WHERE username = :username AND password = :password LIMIT 1")
   LiveData<User> getUserLogin(String username, String password);
 
+  //Retrieve a user by userId (observable)
   @Query("SELECT * FROM " + RockPaperScissorsDatabase.USER_TABLE +
       " WHERE userId = :id LIMIT 1")
   LiveData<User> getUserById(int id);
 
+  //Update a user's username
   @Query("UPDATE " + RockPaperScissorsDatabase.USER_TABLE +
       " SET username = :newUsername WHERE userId = :userId")
   int updateUsername(int userId, String newUsername);
 
+//Update a user's password
   @Query("UPDATE " + RockPaperScissorsDatabase.USER_TABLE +
       " SET password = :newPassword WHERE userId = :userId")
   int updatePassword(int userId, String newPassword);
@@ -37,11 +47,12 @@ public interface UserDAO {
   @Query("SELECT * FROM " + RockPaperScissorsDatabase.USER_TABLE)
   LiveData<List<User>> getAllUsers();
 
-  //TODO implement delete method
+  // Delete a user by username
   @Query("DELETE FROM " + RockPaperScissorsDatabase.USER_TABLE +
       " WHERE username = :username")
   void deleteUserByUsername(String username);
 
+  //Synchronous user lookup (used in testing or validation)
   @Query("SELECT * FROM " + RockPaperScissorsDatabase.USER_TABLE +
       " WHERE username = :username LIMIT 1")
   User getUserByUsernameSync(String username);
